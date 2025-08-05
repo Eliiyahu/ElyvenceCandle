4d86c5f7ccdeb3311b91f3e59b15a9560621ad0a
Eliiyahu <anpantheanpantheanpan@gmail.com>
2025-08-05 17:53:42 +0300

    Update burger.js

--- burger.js
+++ burger.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const burger = document.querySelector(".burger");
-    const navMenu = document.querySelector(".top-menu");
+  const burger = document.querySelector(".burger");
+  const navMenu = document.querySelector(".top-menu");
+  const overlay = document.querySelector(".overlay");
 
-    if (burger && navMenu) {
-        burger.addEventListener("click", () => {
-            navMenu.classList.toggle("show");
-            burger.classList.toggle("active");
-        });
-    }
+  if (burger && navMenu && overlay) {
+    burger.addEventListener("click", () => {
+      burger.classList.toggle("active");
+      navMenu.classList.toggle("show");
+      overlay.classList.toggle("active");
+    });
+
+    overlay.addEventListener("click", () => {
+      burger.classList.remove("active");
+      navMenu.classList.remove("show");
+      overlay.classList.remove("active");
+    });
+  }
 });
