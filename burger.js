fffb371a199f40dd46a4dd026ef733c2f680b083
Eliiyahu <anpantheanpantheanpan@gmail.com>
2025-08-05 15:28:21 +0300

    Create burger.js

---
+++ burger.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const navMenu = document.querySelector(".top-menu");
+
+    if (burger && navMenu) {
+        burger.addEventListener("click", () => {
+            navMenu.classList.toggle("open");
+            burger.classList.toggle("active");
+        });
+    }
+});
